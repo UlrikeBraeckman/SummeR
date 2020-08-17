d51fdd22c43bf1a227b9c551cb4759ee8ae685e7
stanleesocca <nmoreloaded4show@ymail.com>
2020-08-17 23:42:36 +0200

    Modify data handling 3 yaml

--- Data_Handling 1/change_directory2parent.R
+++ Data_Handling 1/change_directory2parent.R
@@ -2,8 +2,8 @@
 # it is easy to naviagte from the parent directory into the individual lesson directory
 
 # if the current directory is inside Data_Handling folder, set directory to the parent.
-curdir <- basename(getwd())
-if(curdir %in% c("Data_Handling", "Data_Handling_part_2", "Data_Handling_part_3")) {
-  setwd("../")
-}
-rm(curdir)
+# curdir <- basename(getwd())
+# if(curdir %in% c("Data_Handling", "Data_Handling_part_2", "Data_Handling_part_3")) {
+#   setwd("../")
+# }
+# rm(curdir)
--- Data_Handling/Data_Handling 2/number_index_illustrated.R
+++
@@ -1,7 +0,0 @@
-plot(1:15, 1:15, type = "n", axes = FALSE, ann = FALSE)
-rect(xleft = 2, ybottom = 7, xright = 14, ytop = 10)
-rect(2.1, 7, 5, 9.9, col = "grey90", border = NA)
-segments(x0 = c(5,8,11,14), y0 = 7, x1 = c(5,8,11,14), y1 = 10, lty = 2)
-text(c(3.5, 6.5, 9.5, 12.5), 8.5, labels = c("Whale", "Gobies", "Ray", "Turtle"))
-text(x = 3.5, y = 5.5, labels = "Ocean_animal [1]")
-
