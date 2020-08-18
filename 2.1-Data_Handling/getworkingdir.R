a5ca4d3db157480771a42591bcb8133cfec43c91
stanleesocca <nmoreloaded4show@ymail.com>
2020-08-18 16:38:07 +0200

    commented out. Not needed anymore. All changes moved to initlesson.R

---
+++ 2.1-Data_Handling/getworkingdir.R
@@ -0,0 +1,11 @@
+# ensure the directory that the student first is get to is the parent directory, so 
+# it is easy to naviagte from the parent directory into the individual lesson directory
+
+# if the current directory is inside Data_Handling folder, set directory to the parent.
+# curdir <- basename(getwd())
+# if(curdir %in% c("Data_Handling", "Data_Handling_part_2", "Data_Handling_part_3")) {
+#   setwd("../")
+# }
+# rm(curdir)
+
+# curdir <- getwd()
