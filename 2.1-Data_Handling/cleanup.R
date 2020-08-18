1daca93a738ff6451e2da8983b366b1bdd4fb856
stanleesocca <nmoreloaded4show@ymail.com>
2020-08-18 16:37:44 +0200

    modify to redirect user back to their former R state

--- 2.1-Data_Handling/cleanup.R
+++ 2.1-Data_Handling/cleanup.R
@@ -1,2 +1,5 @@
-rm(list = ls())
-setwd("../")
+# reset the working directory back to the state the student started with.
+setwd(olddir)
+
+# clean up all the variable created in the workspace
+rm(list = ls(), inherits = TRUE)
