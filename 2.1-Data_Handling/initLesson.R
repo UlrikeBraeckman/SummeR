e17cd05ccacf7e497498fb16a11efeeeee8542f9
stanleesocca <nmoreloaded4show@ymail.com>
2020-08-18 16:38:37 +0200

    Get old dir, and set new one that point to R course

--- 2.1-Data_Handling/initLesson.R
+++ 2.1-Data_Handling/initLesson.R
@@ -2,10 +2,13 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
-.get_course_path <- function(){
-  tryCatch(swirl:::swirl_courses_dir(),
-           error = function(c) {file.path(find.package("swirl"),"Courses", "SummeR")}
-  )
-}
+olddir <- getwd()
+assign("olddir", olddir, envir = .GlobalEnv)
+
+# .get_course_path <- function(){
+#   tryCatch(swirl:::swirl_courses_dir(),
+#            error = function(c) {file.path(find.package("swirl"),"Courses", "SummeR")}
+#   )
+# }
 
 setwd(file.path(find.package("swirl"),"Courses", "SummeR"))
